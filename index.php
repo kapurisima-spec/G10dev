6b351d763683f27bad3f86027a1c843253f39160
kapurisima-spec <kapurisima@cpnc.edu.ph>
2026-07-02 15:04:23 +0800

    types of variable

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'Hello github';
+echo  'hello github'. "<br/>";
+echo 'success'; 
+
+// string variable 
+$name = "this is a string";
+
+// number variable 
+$number = 1;
+
+//boolean 
+$isSingle = false;
+
+//floating
+$float = 1.5;
+
